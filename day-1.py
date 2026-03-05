1f3473e89806a1b2d5f5352911019e6f6e20ce41
karthik8626 <kothakarthik8626@gmail.com>
2026-03-05 19:29:07 +0530

    Update day-1.py

--- day-1.py
+++ day-1.py
@@ -1,22 +1,15 @@
 print("hello world")
 '''Program 2 — Sum of two numbers
-
 Input 2 numbers and print sum.
-
 Program 3 — Simple calculator
-
 Add
 Subtract
 Multiply
 Divide
-
 Program 4 — Age calculator
-
 Input birth year
-Output current age.
-
+Output current age.2
 Program 5 — Even or Odd
-
 Input a number
 Check if even or odd.'''
 num1=int(input("enter number"))
@@ -26,6 +19,13 @@ print(num1-num2)
 print(num1*num2)
 print(num1/num2)
 
-year=int(input("enter year"))
-age=2026-year
+num=int(input("enter number to know even or odd"))
+if num % 2==0:
+    print("even")
+else:
+    print("odd")
+    
+year=int(input("enter birth year"))
+curyear=int(input("enter current year"))
+age=curyear-year
 print(age)

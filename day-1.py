3e0630bd774535a1eff6cc9d5e6269ad19847581
karthik8626 <kothakarthik8626@gmail.com>
2026-03-05 19:16:57 +0530

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,31 @@
+print("hello world")
+'''Program 2 — Sum of two numbers
+
+Input 2 numbers and print sum.
+
+Program 3 — Simple calculator
+
+Add
+Subtract
+Multiply
+Divide
+
+Program 4 — Age calculator
+
+Input birth year
+Output current age.
+
+Program 5 — Even or Odd
+
+Input a number
+Check if even or odd.'''
+num1=int(input("enter number"))
+num2=int(input("enter number"))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+year=int(input("enter year"))
+age=2026-year
+print(age)
